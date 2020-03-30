c7cf4d14e6364eca9e37f5fa01879b4f98d24efa
isuc-76/76 <eriasimoyase@gmail.com>
2020-03-30 18:04:12 +0900

    add_markets

---
+++ app/controllers/admin/markets_controller.rb
@@ -0,0 +1,27 @@
+class Admin::MarketsController < ApplicationController
+
+	def tukinomiya
+		@market = Market.find(1)
+	end
+
+	def katori
+		@market = Market.find(2)
+	end
+
+	def update
+		@market = Market.find(params[:id])
+		if @market.update(market_params)
+			redirect_back(fallback_location: root_path)
+		else
+			redirect_back(fallback_location: root_path)
+		end
+	end
+
+
+	private
+
+		def market_params
+			params.require(:market).permit(:day, :content)
+		end
+
+end
